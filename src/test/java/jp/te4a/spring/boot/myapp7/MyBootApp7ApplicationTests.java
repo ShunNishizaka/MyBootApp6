050ecba08025a4f4fcfb546a9f91ee633bbd8b15
西坂 瞬 <180581@jc-21.jp>
2021-09-27 12:26:44 +0900

    iroiro

--- src/test/java/jp/te4a/spring/boot/myapp7/MyBootApp7ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp7/MyBootApp7ApplicationTests.java
@@ -1,13 +1,17 @@
 package java.jp.te4a.spring.boot.myapp7;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class MyBootApp7ApplicationTests {
 
-//	@Test
+	@Test
 	void contextLoads() {
+		HelloController helloController = new HelloController();
+		String expect  ="books/list"";
+		String actual = helloController.index();
+		assertEquals(expect, actual);
 	}
 
 }
